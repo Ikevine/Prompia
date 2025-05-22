e5784857293713cea4697332ebe9a9a7313ecd51
Ikevine <ishimwekevine098@gmail.com>
2025-05-23 00:34:24 +0200

    allow copying data to clipboard

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+'use client'
 
+import { useState, useEffect} from 'react';
+import PrompCard from './prompCard'
+
+//This component is only going to be used in here //Function return the html tag
+const PrompCardList = ({data, handleTagClick}) => {
+ return(
+  <div className="mt-16 prompt_layout">
+     {
+      data.map((post) =>(
+        <PrompCard 
+         key={post._id}
+         post={post}
+         handleTagClick={handleTagClick}
+        />
+      ))
+     }
+ </div>
+ );
+};
+
+// Feed function start here
 const Feed = () => {
+  const [searchText, setsearchText] = useState('');
+
+  const [posts, setposts] = useState([])
+  
+  const handleSearchChange = (e) =>{
+      // get request
+  }
+
+  useEffect(() => {
+    const fetchPosts = async()=>{
+    const response = await fetch('/api/prompt');
+    const data = await response.json();
+
+    setposts(data)
+    }
+
+    fetchPosts();
+  }, [])
+  
   return (
-    <div>Feed</div>
+    <section className='feed'>
+        <form className='relative w-full fle-center'>
+          <input 
+          type="text" 
+          placeholder='Search for tag or username'
+          onChange={handleSearchChange}
+          className='peer search_input'
+          />
+        </form>
+
+
+        <PrompCardList
+          data = {posts}
+          handleTagClick = {()=>{}}
+        />
+    </section>
   )
 }
 
